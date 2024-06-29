7ca2e63601e565026ff82cb7468c1db0ca81fd8a
Sophia Gibson <73716087+sophia-gibson@users.noreply.github.com>
2024-06-29 15:43:58 -0700

    Update SLC29A3_modbamtools.sh

--- Methylation Analysis/SLC29A3_modbamtools.sh
+++ Methylation Analysis/SLC29A3_modbamtools.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-BAM1=/n/1000g/align-card-2.24-hg38/FIRST_100/HG02389.LSK110.R9/HG02389.LSK110.R9_PMDV_FINAL.haplotagged.bam
-BAM2=/n/1000g/align-card-2.24-hg38/FIRST_100/HG03022.LSK110.R9/HG03022.LSK110.R9_PMDV_FINAL.haplotagged.bam
-GTF=/n/users/sgibson/1000g_preprint/methylation/figs/gencode.v38.annotation.sorted.gtf.gz
-OUT=/n/users/sgibson/1000g_preprint/methylation/figs
+BAM1=HG02389.LSK110.R9_PMDV_FINAL.haplotagged.bam
+BAM2=HG03022.LSK110.R9_PMDV_FINAL.haplotagged.bam
+GTF=gencode.v38.annotation.sorted.gtf.gz
+OUT=$1
 
 cd $OUT
 
